b6124e588ea3356a5c3fc07d7225cf0f0e45f4c2
Jacob Peterson <petersonjacobw@gmail.com>
2021-03-16 20:41:45 -0700

    Added comment for discussion tomorrow before expanding

--- server/API.js
+++ server/API.js
@@ -6,23 +6,25 @@ const helper = require('./api_helper.js');
 const app = express();
 const port = 3001;
 
-const reviewsService = 'http://localhost:3000/';
+const reviewsService = 'http://localhost:3000/reviews';
 
 app.use(express.json());
 
 app.get('/:endpoint', function (req, res) {
   const destination = req.params.endpoint;
   const product_id = req.query.product_id;
-  // if (destination === 'reviews') {
-  //   helper.fetchData(reviewsService, destination, { params: { product_id: product_id, sort: 'relevant', count: 100 } }, console.log('hello'))
-  // }
+
+  //  I think this can be made very DRY by if-clausing the endpoint to set the options.url and options.params
+  //  so that only one is needed for all GET requests, or is it more performant to have one handler for each
+  //  major service? Presumably the latter
+  
   var options = {
     method: 'GET',
-    url: reviewsService + destination,
+    url: reviewsService,
     params: { product_id: product_id },
   }
   axios(options)
-  .then((response) => console.log('success', response.data))
+  .then((response) => console.log(response.data))
   .catch((err) => console.log(err))
 
   res.status(200).send(`${destination} and ${product_id}`)
